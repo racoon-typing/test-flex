1fb93b64bbaa628d7169a86eb7e007fe5cfcb99c
Daniil <eo18622@yandex.ru>
2022-08-09 15:57:02 +0300

    форматирует

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,10 +1,10 @@
 import fileinclude from "gulp-file-include";
 
 export const html = () => {
-    return app.gulp.src(app.path.src.html)
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '@file',
-          }))
-        .pipe(app.gulp.dest(app.path.build.html))
+  return app.gulp.src(app.path.src.html)
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file',
+    }))
+    .pipe(app.gulp.dest(app.path.build.html))
 }

4c13a4399666fe5046bcd3056441b67fe9a84339
Daniil <eo18622@yandex.ru>
2022-08-14 11:22:17 +0300

    каунтер

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,5 +1,6 @@
 import fileinclude from "gulp-file-include";
-import webpHtmlNosvg from "gulp-webp-html-nosvg"
+import webpHtmlNosvg from "gulp-webp-html-nosvg";
+import versionNumber from "gulp-version-number";
 
 export const html = () => {
   return app.gulp.src(app.path.src.html)
@@ -9,5 +10,21 @@ export const html = () => {
     }))
     .pipe(app.plugins.replace(/@img\//g, 'img/'))
     .pipe(webpHtmlNosvg())
+    .pipe(
+      versionNumber({
+        'value': '%DT%',
+        'append': {
+          'key': '_v',
+          'cover': 0,
+          'to': [
+            'css',
+            'js',
+          ]
+        },
+        'output': {
+          'file': 'gulp/version.json'
+        }
+      })
+    )
     .pipe(app.gulp.dest(app.path.build.html))
 }

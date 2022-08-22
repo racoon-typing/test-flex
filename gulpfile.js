c076f36e4f6e5237b57a5305916b76c2440c8da8
Daniil <eo18622@yandex.ru>
2022-08-22 10:32:41 +0300

    добавляет шрифты

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js"; 
 import { js } from "./gulp/tasks/js.js"; 
 import { images } from "./gulp/tasks/images.js"; 
-
+import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js"
 
 // Наблюдатель за изменениями файлов
 function watcher() {
@@ -31,7 +31,9 @@ function watcher() {
     gulp.watch(path.watch.images, images)
 } 
 
-const mainTasks = gulp.parallel(copy, html, scss, js, images)
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
 
 // Построение сценариев выполенения задач 
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
